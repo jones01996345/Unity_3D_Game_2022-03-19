c1a1421be05ae60c2f188845bd088e65cf17857f
jones01996345 <96329808+jones01996345@users.noreply.github.com>
2022-04-16 17:13:52 +0800

    update_刀光攻擊系統

--- 3D_GAME_20220319/Assets/Scripts/Attack.cs
+++ 3D_GAME_20220319/Assets/Scripts/Attack.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 namespace Jones
 {
     /// <summary>
+    /// 攻擊系統
     /// 1.武器切換
     /// 2.攻擊動畫
     /// 3.刀光特效
@@ -23,8 +24,16 @@ namespace Jones
         private GameObject goWeaponHand;
         [SerializeField, Header("刀光")]
         private ParticleSystem psLight;
+        [SerializeField,Header("第一階段收刀時間")]
+        private float timeSwordToBack=2.5f;
+        [SerializeField, Header("第二階段收刀時間")]
+        private float timeSwordToHide = 3.5f;
 
         private string paramateterAttack = "觸發攻擊";
+        private bool isAttack;
+        private bool isBack;
+        private float timer;
+        private float timerToHide;
         #endregion
         #region 事件
         private void Awake()
@@ -34,6 +43,8 @@ namespace Jones
         private void Update()
         {
             SwitchWeapon();
+            SwordToBack();
+            SwordToHide();
         }
         #endregion
 
@@ -48,10 +59,47 @@ namespace Jones
                 goWeaponBack.SetActive(false);
                 goWeaponHand.SetActive(true);
 
-                ani.SetTrigger(paramateterAttack);
-                psLight.Play();
+                ani.SetTrigger(paramateterAttack);      //觸發攻擊動畫
+                psLight.Play();                         //播放攻擊特效
+
+                timer = 0;                              //每次攻擊計時重算
+                isAttack = true;
             }
 
+        }
+        /// <summary>
+        /// 第一階段:刀子從手上收回背後
+        /// </summary>
+        private void SwordToBack()
+        {
+            if(isAttack)
+            {
+                timer += Time.deltaTime;
+                if(timer>=timeSwordToBack)
+                {
+                    goWeaponHand.SetActive(false);
+                    goWeaponBack.SetActive(true);
+
+                    timer = 0;                      //收刀後計時器歸零
+                    isAttack = false;
+                    isBack = true;
+                }
+            }
+        }
+        /// <summary>
+        /// 收刀第二階段:從背後隱藏
+        /// </summary>
+        private void SwordToHide()
+        {
+            if(isBack)
+            {
+                timerToHide += Time.deltaTime;
+                if(timerToHide>=timeSwordToHide)
+                {
+                    goWeaponBack.SetActive(false);
+                    timerToHide = 0;
+                }
+            }
         }
         #endregion
 
